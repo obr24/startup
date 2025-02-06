a654f2ceba6aad571e6fa07db065abfe03ab4c7c
Orion <obr24@byu.edu>
2025-02-06 08:50:58 -0700

    Update all files so basic npm runs

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import './app.css';     // TODO remove
+
+export default function App() {
+  return <div className='body bg-dark text-light'>App will display here</div>;
+}
