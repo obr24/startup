19ee356bcfafcc363ed89eca0ebc2be959b067c1
Orion <obr24@byu.edu>
2025-02-06 10:25:31 -0700

    Update inspiration.html to work

--- src/app.jsx
+++ src/app.jsx
@@ -20,9 +20,9 @@ export default function App() {
                     </a>
                     <div className="mt-0">Welcome, Jeff</div>
                 </div>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                {/*<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
-                </button>
+                </button>*/}
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
