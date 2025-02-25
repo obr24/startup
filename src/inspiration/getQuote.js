6ce52ba9e4141fdddaedbe9160e811a64139d258
Orion <obr24@byu.edu>
2025-02-24 20:59:57 -0700

    Update inspiration page with api mockup

---
+++ src/inspiration/getQuote.js
@@ -0,0 +1,18 @@
+export default function getQuote() {
+  console.log("hi");
+  var ret = [
+    {
+      q: "Keep your nose out the sky, keep your heart to god, and keep your face to the raising sun.",
+      a: "Kanye West",
+      h: "<blockquote>&ldquo;Remember that sometimes not getting what you want is a wonderful stroke of luck.&rdquo; &mdash; <footer>Dalai Lama</footer></blockquote>",
+    },
+  ];
+  console.log(ret);
+  return [
+    {
+      q: "Keep your nose out the sky, keep your heart to god, and keep your face to the raising sun.",
+      a: "Kanye West",
+      h: "<blockquote>&ldquo;Remember that sometimes not getting what you want is a wonderful stroke of luck.&rdquo; &mdash; <footer>Dalai Lama</footer></blockquote>",
+    },
+  ];
+}
--- src/inspiration/inspiration.jsx
+++ src/inspiration/inspiration.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import getQuote from './getQuote.js'
 
 export function Inspiration() {
+  const [quoteAPIReturn, setAPIReturn] = React.useState('Loading...');
+  const [quote, setQuote] = React.useState('Loading...');
+  const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
+  
+  React.useEffect(() => {
+    setAPIReturn(getQuote())
+    setQuote('Keep your nose out the sky, keep your heart to god, and keep your face to the raising sun.');
+    setQuoteAuthor('Ye');
+  }, []);
+
   return (
     <main>
         <figure className="mx-auto mt-5 mb-5 justify-content-center overflow-auto" style={{"width":"400px"}}>
             <img src="quote-symbol.svg"></img>
             <blockquote className="blockquote">
-              <p>Keep your nose out the sky, keep your heart to god, and keep your face to the raising sun.</p>
+              {/* <p>Keep your nose out the sky, keep your heart to god, and keep your face to the raising sun.</p> */}
+              {/* <p>{quote}</p> */}
+              <p>{quoteAPIReturn[0].q}</p>
             </blockquote>
-            <figcaption className="blockquote-footer">Ye</figcaption>
+            {/* <figcaption className="blockquote-footer">Ye</figcaption> */}
+            <figcaption className="blockquote-footer">{quoteAPIReturn[0].a}</figcaption>
           </figure>
     </main>
   );
