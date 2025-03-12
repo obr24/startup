4eea13b41920a52825178ce554685d74fac9e8fa
Orion <obr24@byu.edu>
2025-03-12 17:01:29 -0600

    Update for login backend. file: src/inspiration/inspiration.jsx

--- src/inspiration/inspiration.jsx
+++ src/inspiration/inspiration.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import getQuote from './getQuote.js'
 
 export function Inspiration() {
-  const [quoteAPIReturn, setAPIReturn] = React.useState('Loading...');
+  const [quoteAPIReturn, setAPIReturn] = React.useState({quote: "loading", author: "loading Author"});
   const [quote, setQuote] = React.useState('Loading...');
   const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
   
   React.useEffect(() => {
-    setAPIReturn(getQuote())
-    setQuote('Keep your nose out the sky, keep your heart to god, and keep your face to the raising sun.');
-    setQuoteAuthor('Ye');
+  async function fetchQuote() {
+    try {
+      const data = await getQuote();
+      setAPIReturn(data);
+    } catch (error) {
+      console.error("Error getting quote: ", error);
+    }
+  }
+    fetchQuote();
   }, []);
 
   return (
@@ -17,12 +23,9 @@ export function Inspiration() {
         <figure className="mx-auto mt-5 mb-5 justify-content-center overflow-auto" style={{"width":"400px"}}>
             <img src="quote-symbol.svg"></img>
             <blockquote className="blockquote">
-              {/* <p>Keep your nose out the sky, keep your heart to god, and keep your face to the raising sun.</p> */}
-              {/* <p>{quote}</p> */}
-              <p>{quoteAPIReturn[0].q}</p>
+              <p>{quoteAPIReturn.quote}</p>
             </blockquote>
-            {/* <figcaption className="blockquote-footer">Ye</figcaption> */}
-            <figcaption className="blockquote-footer">{quoteAPIReturn[0].a}</figcaption>
+            <figcaption className="blockquote-footer">{quoteAPIReturn.author}</figcaption>
           </figure>
     </main>
   );
