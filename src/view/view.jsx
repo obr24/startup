75263967b142afd048bd63631aabb7926c114de6
Orion <obr24@byu.edu>
2025-04-01 18:34:19 -0600

    Websocket now fully working. file: src/view/view.jsx

--- src/view/view.jsx
+++ src/view/view.jsx
@@ -1,8 +1,8 @@
 import React, { useContext, useEffect } from 'react';
 
 import RecipesContext, {ProvideRecipesContext, Recipe} from '../recipes';
-// import { LikeEvent, LikeNotifier } from './likeNotifier';
-import { LikeEvent, LikeEventNotifier } from './likeNotifier';
+import { LikeEvent, LikeNotifier } from './likeNotifier';
+// import { LikeEvent, LikeEventNotifier } from './likeNotifier';
 
 export function View() {
   return (
@@ -19,7 +19,7 @@ function MainContent() {
         } = useContext(RecipesContext);
 
     useEffect(() => {
-        const LikeNotifier = new LikeEventNotifier();
+        //const LikeNotifier = new LikeEventNotifier();
         LikeNotifier.addHandler(handleServerLike);
         
         LoadRecipes();
